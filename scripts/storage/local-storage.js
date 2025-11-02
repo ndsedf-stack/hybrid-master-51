325bb373ff49c30851a7b0eab951675394713bcd
ndsedf-stack <ndsedf@gmail.com>
2025-11-02 03:22:34 +0100

    Implement local storage manager functions

--- scripts/storage/local-storage.js
+++ scripts/storage/local-storage.js
@@ -1,1 +1,17 @@
+// ==================================
+// LOCAL STORAGE MANAGER
+// ==================================
+const key = "hm51_user_data";
 
+export function saveData(data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+export function loadData() {
+  const raw = localStorage.getItem(key);
+  return raw ? JSON.parse(raw) : null;
+}
+
+export function clearData() {
+  localStorage.removeItem(key);
+}
